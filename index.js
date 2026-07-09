6adf610b3e307e27225f62ad89e55e07d60652cc
Jardimjose <jardim.jose@outlook.com>
2026-07-09 19:55:02 +0200

    styled the navbar + css implemented

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-    res.send("Heloo world");
+    res.render('pages/home', { pageTitle: 'Starbucks - Home', pageStyle: "Home" });
 })
 
+app.get("/about", (req, res) => {
+    res.render("pages/about", { pageTitle: "Starbucks - About" });
+})
+
+
 app.listen(PORT, () => {
-    console.log(`Server is now listening on port: ${PORT}`);
+    console.log(`Server is now live on: http://127.0.0.1:${PORT}/`);
 })
